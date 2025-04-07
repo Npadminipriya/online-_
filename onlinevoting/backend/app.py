f744e2f586b4559a3f1cbc07acf1fff69518c680
Npadminipriya <nandamudiharika@gmail.com>
2025-04-07 18:40:09 +0530

    Update app.py

--- onlinevoting/backend/app.py
+++ onlinevoting/backend/app.py
@@ -160,4 +160,6 @@ def cast_vote():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()  # Ensure database tables are created
-    app.run(debug=True, port=5000)
+    #app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
